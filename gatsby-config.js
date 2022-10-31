e7847296371e03678245812f855402e69b915d90
Joshua Balloch <joshuaballoch@gmail.com>
2022-10-31 10:37:05 +0100

    Basic builder setup working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,25 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: `Thimble Consulting`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://thimble.consulting`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap"]
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-sitemap"
+    }, {
+      resolve: '@builder.io/gatsby',
+      options: {
+        publicAPIKey: "464eac5a0492435cb67f71783742bba3",
+        templates: {
+          // Render every `page` model as a new page using the
+          // src/templates/page.jsx template based on the URL provided in Builder.io
+          page: path.resolve('src/templates/page.jsx'),
+        },
+      },
+    }
+  ]
 };
---
+++ src/templates/page.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { BuilderComponent } from '@builder.io/react';
+
+// Your template populates your Gatsby pages with your Builder page content.
+function PageTemplate({ data }) {
+  const models = data?.allBuilderModels;
+  const page = models.page?.[0]?.content;
+
+  return <BuilderComponent name="page" content={page} />;
+}
+
+export default PageTemplate;
+
+// pageQuery is a GraphQL query that
+// fetches each page's content from Builder.
+// Your content is rendered within the
+// PageTemplate using BuilderComponent, provided by Builder's SDK.
+export const pageQuery = graphql`
+  query ($path: String!) {
+    allBuilderModels {
+      page(target: { urlPath: $path }) {
+        content
+      }
+    }
+  }
+`;
