e5d5b74a9d416ba715dbe79794d857c4a6e2bf3d
Joshua Balloch <joshuaballoch@gmail.com>
2024-04-09 16:23:03 +0100

    Remove builder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,9 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
     title: `Thimble Consulting`,
-    siteUrl: `https://thimble.consulting`
+    siteUrl: `https://thimble.consulting`,
   },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-sitemap"
-    }, {
-      resolve: '@builder.io/gatsby',
-      options: {
-        publicAPIKey: "464eac5a0492435cb67f71783742bba3",
-        templates: {
-          // Render every `page` model as a new page using the
-          // src/templates/page.jsx template based on the URL provided in Builder.io
-          page: path.resolve('src/templates/page.jsx'),
-        },
-      },
-    }
-  ]
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap"],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,14 @@
-import { BuilderComponent, builder, useIsPreviewing } from '@builder.io/react';
-import { useEffect, useState } from "react";
-import * as React from "react"
-
-builder.init("464eac5a0492435cb67f71783742bba3");
+import * as React from "react";
 
 const IndexPage = () => {
-  const isPreviewingInBuilder = useIsPreviewing();
-  const [notFound, setNotFound] = useState(false);
-  const [content, setContent] = useState(null);
-
-  // get the page content from Builder
-  useEffect( () => {
-    async function fetchContent() {
-      const content = await builder.get('page', {
-        url: window.location.pathname
-      }).promise();
-
-      setContent(content);
-      setNotFound(!content);
-    }
-    fetchContent();
-  }, []);
-  // if no page is found, return a 404 page
-  if (notFound && !isPreviewingInBuilder) {
-    return <div>Not Found 404 Error</div>
-  }
-
-  // return the page when found
   return (
     <>
-      {/* Render the Builder page */}
-      <BuilderComponent model="page" content={content} />
+      {/* Initial Empty Page */}
+      Hello World!
     </>
   );
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Thimble Consulting</title>
+export const Head = () => <title>Thimble Consulting</title>;
