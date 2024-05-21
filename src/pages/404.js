01ed7b7e653bf2b702ffb0ad52cebd43c05949a5
Joshua Balloch <joshuaballoch@gmail.com>
2024-05-21 11:47:19 +0100

    Fix static site gen (deploy):
    * Needed to add a wrapRootElement thing in
      gatsby-ssr.js to fix static site generator
      having access to the AppContext

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,31 +10,28 @@ const codeStyles = {
   borderRadius: 4,
 };
 
-const NotFoundPage = () => {
-  return (
-    <Layout>
-      <section className="my-8 py-20 bg-ghost-white rounded-lg text-center px-4">
-        <h1 className="text-5xl font-bold font-serif mb-4">Page not found</h1>
-        <p>
-          Whoops! We couldn’t find what you were looking for.
-          <br />
-          {process.env.NODE_ENV === "development"
-            ? (
-              <>
-                <br />
-                Try creating a page in{" "}
-                <code style={codeStyles}>src/pages/</code>.
-                <br />
-              </>
-            )
-            : null}
-          <br />
-          <Link to="/">here's our homepage</Link>
-        </p>
-      </section>
-    </Layout>
-  );
-};
+const NotFoundPage = () => (
+  <Layout>
+    <section className="my-8 py-20 bg-ghost-white rounded-lg text-center px-4">
+      <h1 className="text-5xl font-bold font-serif mb-4">Page not found</h1>
+      <p>
+        Whoops! We couldn’t find what you were looking for.
+        <br />
+        {process.env.NODE_ENV === "development"
+          ? (
+            <>
+              <br />
+              Try creating a page in <code style={codeStyles}>src/pages/</code>.
+              <br />
+            </>
+          )
+          : null}
+        <br />
+        <Link to="/">here's our homepage</Link>
+      </p>
+    </section>
+  </Layout>
+);
 
 export default NotFoundPage;
 
