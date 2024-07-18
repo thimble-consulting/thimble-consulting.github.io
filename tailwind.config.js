a1755b69ff9009774c579d7c863ff47aa59cf666
Joshua Balloch <joshuaballoch@gmail.com>
2024-07-18 15:42:42 +0100

    Add in Daisy UI and upgrade Tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       colors: {
         "walden-green": "#183028",
         primary: {
           lighter: "#4b3f72",
-          default: "#1f2041",
+          DEFAULT: "#1f2041",
           darker: "#4b4c75",
         },
         secondary: {
           lighter: "#3c91e6",
-          default: "#2867a6",
+          DEFAULT: "#2867a6",
           darker: "#174e85",
         },
         "non-photo-blue": "#A631FA",
@@ -27,15 +28,15 @@ module.exports = {
         "raisin-black": "#272932",
         "french-blue": "#0075C4",
         "weblink-blue": {
-          default: "#2f6ca8",
+          DEFAULT: "#2f6ca8",
           darker: "#0f4a85",
         },
         "weblink-visited": {
-          default: "#800080",
+          DEFAULT: "#800080",
         },
         thimble: {
           lighter: "#ed7e7e",
-          default: "#E43A3A",
+          DEFAULT: "#E43A3A",
           darker: "#cf2525",
         },
       },
@@ -53,6 +54,8 @@ module.exports = {
       ],
     },
   },
-  variants: {},
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {},
 };
