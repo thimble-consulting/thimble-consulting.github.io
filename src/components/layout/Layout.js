ae3b8db0be6319789353bc585ae306bc63db9e52
Ollie Randall <ollie@thimble.dev>
2024-08-02 14:30:00 +0100

    fix duplicate header

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -8,9 +8,6 @@ const commonWidthClasses = "max-w-6xl mx-auto px-4 md:px-6 lg:px-8";
 const Layout = ({ children }) => {
   return (
     <>
-      <Header className={commonWidthClasses} />
-      <main className={`${commonWidthClasses} text-black`}>{children}</main>
-      <Footer className={commonWidthClasses} />
       <AccessibilityWrapper>
         <Header className={commonWidthClasses} />
         <main className={`${commonWidthClasses} text-black`}>{children}</main>
