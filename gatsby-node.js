3d0388220873131187399c4a42a12f459c83b9ca
Ruairi Kerins <ruairikerins95@gmail.com>
2024-09-12 16:36:19 +0100

    Initial semi working programmatically creating pages using createPages API
    - All the content is layered on top of itself need to be fixed up
    
    Co-authored-by: Katherine Bowler <katbow@users.noreply.github.com>

---
+++ gatsby-node.js
@@ -0,0 +1,59 @@
+const path = require("path")
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const clientPageTemplate = path.resolve(`src/templates/clientPage.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  return graphql(`
+    query PastClients {
+      allPastClientsJson {
+        edges {
+          node {
+            name
+            slug
+            heroText
+            logoSrc {
+              id
+              publicURL
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+            sections {
+              title
+              parallaxLayers
+            }
+            foo
+          }
+        }
+      }
+    }
+  `, { limit: 1000 }).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog post pages.
+    result.data.allPastClientsJson.edges.forEach(edge => {
+      createPage({
+        // Path for this page — required
+        path: `${edge.node.slug}`,
+        component: clientPageTemplate,
+        context: {
+          // Add optional context data to be inserted
+          // as props into the page component.
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+          client: edge.node
+        },
+      })
+    })
+  })
+}
---
+++ src/templates/clientPage.js
@@ -0,0 +1,67 @@
+import * as React from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import * as styles from '../styles/styles.module.css'
+import { StaticImage } from "gatsby-plugin-image";
+import Hero from "../components/Parallax/Hero.js";
+import Header from "../components/layout/Header.js";
+import ContactUsForm from "../components/ContactUsForm.js";
+
+export default function Index({pageContext}) {
+  const { client } = pageContext
+
+  return (
+    <Parallax pages={10}>
+      <Header className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8" />
+      <ContactUsForm />
+      <Hero client={client}></Hero>
+      {client.sections.map(section => {
+        return (
+          <>
+            <ParallaxLayer
+              style={{
+                width: "full",
+                display: 'flex',
+                alignItems: 'start',
+                justifyContent: 'center',
+                zIndex: 100,
+              }}
+              sticky={{ start: 1, end: 4.1 }}>
+              <div className="flex bg-gray-100 py-8 px-8 gap-4 align-middle w-full mb-52">
+                <div className="flex-1 flex items-center">
+                  <p className="text-gray-400 text-lg text-center">
+                    {section.title}
+                  </p>
+                </div>
+              </div>
+            </ParallaxLayer>
+
+            {section.parallaxLayers.map((parallaxLayer, index) => {
+              return(
+                <ParallaxLayer
+                  style={{
+                    width: "60%",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    textAlign: "center",
+                    zIndex: 50,
+                  }}
+                  sticky={{ start: 3.11, end: 4.11 }}>
+                  <div className={`flex flex-col justify-around gap-10 mx-20`}>
+                    {parallaxLayer.map(paragraph => {
+                      return(
+                        <div className={`${styles.slideText}`}>
+                          {paragraph}
+                        </div>
+                      )
+                    })}
+                  </div>
+                </ParallaxLayer>
+              )
+            })}
+          </>
+        )
+      })}
+    </Parallax>
+  )
+}
