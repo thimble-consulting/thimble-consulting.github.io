72fe2073c2f85789522f2a3f85294067da48dfad
Ruairi Kerins <ruairikerins95@gmail.com>
2024-09-20 14:39:30 +0100

    Sub Header animation different based on scroll direction - Height of subheader changes after animation needs a fix
    Co-authored-by: Joshua Balloch <joshuaballoch@users.noreply.github.com>

--- src/templates/clientPage.js
+++ src/templates/clientPage.js
@@ -1,6 +1,7 @@
-import React, { useRef, useState, useCallback, useEffect } from 'react'
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
-import { useScroll, animated, useSpring } from '@react-spring/web'
+import React, { useRef, useEffect, useState } from 'react'
+import { usePrevious } from "@uidotdev/usehooks";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { useTransition, animated, useSpringRef } from '@react-spring/web'
 import { GatsbyImage } from "gatsby-plugin-image";
 import Hero from "../components/Parallax/Hero.js";
 import Header from "../components/layout/Header.js";
@@ -8,69 +9,63 @@ import ContactUsForm from "../components/ContactUsForm.js";
 
 export default function Index({pageContext}) {
   const { client } = pageContext
-  let offset = 1
+  const PAGES = 15
+  const OFFSET = 1
+  let relativeOffset = 1
 
   const imgProps = {
     alt: `Logo of past client company, ${client.name}`,
     ...client.logoSrc,
   }
 
-  const logo = ({ alt, publicURL, childImageSharp }) => {
-    if (childImageSharp) {
-      return (
-        <GatsbyImage
-        className="rounded-xl w-full"
-        imgStyle={{
-          objectFit: "scale-down",
-        }}
-        image={childImageSharp.gatsbyImageData}
-        alt={alt}
-        />
-      )
-    } else { // then it's an svg, see https://github.com/gatsbyjs/gatsby/issues/10297#issuecomment-464834529
-      return (
-        <div className="rounded-xl w-full">
-          <img className="rounded-2xl" style={{ height: "100%" }} src={publicURL} alt={alt} />
-        </div>
-      )
+  const parallax = useRef(null);
+
+  const subHeadings = client.sections.map(section => (
+    ({ style }) => <animated.div style={{ ...style }}>{section.title}</animated.div>
+  ))
+
+  const [subHeaderNum, setSubHeaderNum] = useState(0)
+
+  const transRef = useSpringRef()
+  const transitions = useTransition(subHeaderNum, {
+    ref: transRef,
+    keys: null,
+    ...getTransitionConfig(subHeaderNum, usePrevious(subHeaderNum))
+  })
+
+  useEffect(() => {
+    transRef.start()
+  }, [transRef, subHeaderNum])
+
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const height = parallax.current.space
+      const scrollablePages = PAGES - 1 // because you can't scroll past the last page
+      const scrollHeight = height * scrollablePages
+
+      const scrollTop = e.target.scrollTop
+      const percentScrolled = scrollTop / scrollHeight
+      const currentPage = percentScrolled * scrollablePages
+      const sections = client.sections.length
+      const evenSplit = scrollablePages / sections
+
+      client.sections.forEach((section, index) => {
+        if ((currentPage > (evenSplit * index))  && (currentPage < (evenSplit * (index + 1)))) {
+          setSubHeaderNum(index)
+        }
+      });
     }
-  }
+    const container = parallax.current.container.current
+    container.addEventListener('scroll', handleScroll)
 
-  // config 1 - throws param 1 not of type 'Element'
-  const parallaxRef = useRef(null);
-
-  const [textStyles, textApi] = useSpring(() => ({
-    y: '100%',
-  }))
-
-  // const { scrollYProgress } = useScroll({
-  //   container: parallaxRef
-  // })
-
-  // useEffect(() => {
-  //   const parallax = parallaxRef.current;
-  //   if (parallax) {
-  //     const { scrollYProgress } = useScroll({
-  //       container: parallaxRef
-  //     })
-  //   }
-  // }, [useScroll]);
-
-  // config 2 - works, will jump to page index
-  // const [parallaxRef, setParallaxRef] = useState(null);
-
-  // const handleButtonClick = (pageIndex) => {
-  //   if (parallaxRef) {
-  //     parallaxRef.scrollTo(pageIndex);
-  //   }
-  // };
-
+    return () => {
+      container.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
-    // config 2
-    // <Parallax pages={15} ref={setParallaxRef}>
-    // {/* config 1 */}
-    <Parallax pages={15} ref={parallaxRef}>
+    <Parallax pages={PAGES} ref={parallax}>
       <Header className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8" />
       <ContactUsForm />
       <Hero client={client}></Hero>
@@ -89,63 +84,30 @@ export default function Index({pageContext}) {
       >
         {logo(imgProps)}
       </ParallaxLayer>
+
+      <ParallaxLayer
+        style={{
+          width: "full",
+          display: 'flex',
+          alignItems: 'start',
+          justifyContent: 'center',
+          zIndex: 20,
+        }}
+        className="subHeader"
+        sticky={{ start: OFFSET, end: PAGES }}>
+        <div className="flex bg-gray-100 py-8 px-8 gap-4 align-middle w-full mb-52">
+          <div className="flex-1 flex items-center">
+            {transitions((style, i) => {
+              const Page = subHeadings[i]
+              return <Page style={style} />
+            })}
+          </div>
+        </div>
+      </ParallaxLayer>
       {client.sections.map(section => {
-        offset = offset + section.parallaxLayers.length;
+        relativeOffset = relativeOffset + section.parallaxLayers.length;
         return (
           <>
-            <ParallaxLayer
-              style={{
-                width: "full",
-                display: 'flex',
-                alignItems: 'start',
-                justifyContent: 'center',
-                zIndex: offset,
-              }}
-              className="subHeader"
-              sticky={{ start: (offset - section.parallaxLayers.length), end: offset }}>
-              <div className="flex bg-gray-100 py-8 px-8 gap-4 align-middle w-full mb-52">
-                <div className="flex-1 flex items-center">
-                    {section.title}
-                    {/* config 2 */}
-                    {/* <button onClick={() => handleButtonClick(7)}>Go to Page 8</button> */}
-                </div>
-              </div>
-            </ParallaxLayer>
-
-            <ParallaxLayer
-              sticky={{ start: (offset - section.parallaxLayers.length), end: offset }}
-              style={{
-                width: "40%",
-                inset: "0% 0% 0% 100%",
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'flex-end',
-                justifySelf: 'end',
-                zIndex: offset,
-                right: 0,
-              }}
-            >
-              {logo(imgProps)}
-            </ParallaxLayer>
-
-
-            {/* config 1 */}
-              {/* <animated.div
-                className="dot"
-                style={{
-                  clipPath: scrollYProgress.to(val => `circle(${val * 100}%)`),
-                }}>
-                <h1 className="title">
-                  <span>
-                    <animated.span style={textStyles}>Aha!</animated.span>
-                  </span>
-                  <span>
-                    <animated.span style={textStyles}>You found me!</animated.span>
-                  </span>
-                </h1>
-              </animated.div> */}
-            {/* config 1 */}
-
             {section.parallaxLayers.map((parallaxLayer, index) => {
               return(
                 <ParallaxLayer
@@ -155,7 +117,7 @@ export default function Index({pageContext}) {
                     alignItems: 'center',
                     justifyContent: 'center',
                     textAlign: "center",
-                    zIndex: offset - 1,
+                    zIndex: relativeOffset - 1,
                   }}
                   sticky={{
                     start: parallaxLayer.parallaxParams.stickyStart,
@@ -179,9 +141,47 @@ export default function Index({pageContext}) {
               )
             })}
           </>
-
         )
       })}
     </Parallax>
   )
 }
+
+
+const logo = ({ alt, publicURL, childImageSharp }) => {
+  if (childImageSharp) {
+    return (
+      <GatsbyImage
+        className="rounded-xl w-full"
+        imgStyle={{
+          objectFit: "scale-down",
+        }}
+        image={childImageSharp.gatsbyImageData}
+        alt={alt}
+      />
+    )
+  } else { // then it's an svg, see https://github.com/gatsbyjs/gatsby/issues/10297#issuecomment-464834529
+    return (
+      <div className="rounded-xl w-full">
+        <img className="rounded-2xl" style={{ height: "100%" }} src={publicURL} alt={alt} />
+      </div>
+    )
+  }
+}
+
+
+const getTransitionConfig = (subHeaderNum, previousSubHeader) => {
+  if (subHeaderNum > previousSubHeader) {
+    return {
+      enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+      from: { position: 'absolute', opacity: 0, transform: 'translate3d(100%,0,0)' },
+      leave: { opacity: 0, transform: 'translate3d(-100%,0,0)' },
+    };
+  } else if (subHeaderNum < previousSubHeader) {
+    return {
+      enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+      from: { position: 'absolute', opacity: 0, transform: 'translate3d(-100%,0,0)' },
+      leave: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    };
+  }
+};
